168bff7a18b91d3c6d5c6bc1b1585e635c1aa3fc
Massimo Artizzu <maxart.x@gmail.com>
2025-10-28 15:45:04 +0100

    Add support to duration/delay effect modifiers

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,7 +1,14 @@
 declare namespace astroHTML.JSX {
   interface HTMLAttributes {
+		/** Fragment/slide transition effect */
 		effect?: string;
+		/** Fragment/slide transition duration multiplier (default: 1) */
+		duration?: string;
+		/** Fragment/slide transition delay multiplier (default: 0) */
+		delay?: string;
+		/** Sets the element as a flex container with the given flow specifier (default: row) */
 		flex?: string | boolean;
+		/** Sets the element as a grid container with the given equally wide columns */
 		grid?: string | boolean;
 		w?: string;
 		h?: string;
